dc5c9a600fa539cd6fd5b0966caa942d45973eb7
Paul Borawski <paulborawski14@gmail.com>
2018-03-08 17:25:58 -0800

    connecting state to build controls and now you can add ingredients to the burger

--- burgerbuilder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burgerbuilder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -28,6 +28,9 @@ class BurgerIngredient extends Component {
             case ('salad'):
                 ingredient = <div className={classes.Salad}></div>;
                 break;
+            case ('bacon'):
+                ingredient = <div className={classes.Bacon}></div>;
+                break;
             default:
        ingredient = null;
   }
